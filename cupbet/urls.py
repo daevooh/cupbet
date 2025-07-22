f11fe87009bc19cbf261f2874604ce55ca75c8ed
daevooh <ogbekingdavid@outlook.com>
2025-07-22 10:46:50 +0100

    media files works in prodiction

--- cupbet/urls.py
+++ cupbet/urls.py
@@ -27,3 +27,13 @@ urlpatterns = [
 # Serve media files during development
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)  # Add this for static files
+else:
+    from django.views.static import serve
+    from django.urls import re_path
+
+    urlpatterns += [
+        re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
+        re_path(r'^static/(?P<path>.*)$ ', serve, {'document_root': settings.STATIC_ROOT}),  # Explicitly serve static files
+    ]
+
